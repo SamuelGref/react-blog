bea8cd877f8b1389012399a0b1b283326a51eca0
Samuel Gref <samgref@gmail.com>
2021-09-28 11:50:35 -0400

    new post component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <div>
       <TopBar />
 
-      <Single />
+      <Write/>
     </div>
   );
 }
