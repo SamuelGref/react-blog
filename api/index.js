05697073c773fc1f53a0a3c9cd9769175eb4f256
Samuel Gref <samgref@gmail.com>
2021-10-01 16:30:16 -0400

    GET adn POST Categories

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json());
@@ -16,6 +19,9 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running.");
